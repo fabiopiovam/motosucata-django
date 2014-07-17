044867c111729c0f08a77307c4ef678ee10bfcc8
laborautonomo <fabio@laborautonomo.org>
2014-07-17 19:51:42 -0300

    [fix] Static file settings

--- motosucata/settings.py
+++ motosucata/settings.py
@@ -74,11 +74,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
